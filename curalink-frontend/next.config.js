eb0fd56299d82954179342f93411e3c0f2196870
Utsavcoder1 <149764524+Utsavcoder1@users.noreply.github.com>
2025-11-06 00:33:05 +0530

    Fix: Tailwind CSS paths for GitHub Pages

--- curalink-frontend/next.config.js
+++ curalink-frontend/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
   basePath: '/Curalink',
   assetPrefix: '/Curalink/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
